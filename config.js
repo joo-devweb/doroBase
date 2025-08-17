682154fee141989bc65b46c13f52f8fffa15fa34
joo-devweb <jojorevanaldi@gmail.com>
2025-08-17 12:53:28 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+export let config = {
+    owner: [
+        { jid: '62895416602000', name: 'Nathan' }
+    ],
+    botName: "Doro",
+    prefix: ".",
+    selfMode: false,
+    bannedGroups: new Set()
+};
